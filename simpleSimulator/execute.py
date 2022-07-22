c4195bbd61e191f86d30f336c0763aa568ba5e4c
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-07-22 21:46:29 +0530

    sim

---
+++ simpleSimulator/execute.py
@@ -0,0 +1,8 @@
+class execute:
+    def __init__(self,arr,dict):
+        self.arr=arr
+        self.n=len(arr)
+        self.dict=dict
+    def exe(self):
+        if(arr[0]=='add'):
+                        
