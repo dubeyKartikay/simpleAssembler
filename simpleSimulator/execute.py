6ecf25c1a231bb9aa70d1e6aa153cbdad71a1f2f
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-07-22 22:18:44 +0530

    sim

--- simpleSimulator/execute.py
+++ simpleSimulator/execute.py
@@ -3,6 +3,32 @@ class execute:
         self.arr=arr
         self.n=len(arr)
         self.dict=dict
+    def bintodec(self,bin):
+        n=len(bin)
+        sum=0
+        bin=bin[::-1]
+        for i in range(n):
+            bin[i]=int(bin[i])
+            sum=sum+(bin[i]*(2^n))
+        return sum
+    def dectobin(self,dec):
+        j=""
+        while dec>0:
+            k=dec%2
+            dec=dec//2
+            k=str(k)
+            j=j+k
+        j=j[::-1]
+        return j
+        
     def exe(self):
         if(arr[0]=='add'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            c=dectobin(a+b)
+            
+            
+            
+            
                         
