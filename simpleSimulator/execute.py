40e1af470070c9694a87fabc58f8ee7d1e19eff3
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-07-23 00:31:03 +0530

    sim

--- simpleSimulator/execute.py
+++ simpleSimulator/execute.py
@@ -24,21 +24,100 @@ class SIM:
         result+="0"*(16-len(j))
         result+=j
         return result    
-    def execute(self):
+    def execute(self,PC):
         if(arr[0]=='add'):
             a=self.bintodec(arr[1])
             b=self.bintodec(arr[2])
             c=""
             c=dectobin(a+b)
             self.dict[arr[3]]=c    
-        if(arr[0]=='sub'):
+            PC+=1
+        elif(arr[0]=='sub'):
             a=self.bintodec(arr[1])
             b=self.bintodec(arr[2])
             c=""
             c=dectobin(a-b)
             self.dict[arr[3]]=c 
-        if(arr[0]=='mov'):
+            PC+=1
+        elif(arr[0]=='mov'):                  #imm value int me dio naki string!!!
+            if(type(arr[2]==int)):
+                b=""
+                b=self.dectobin(arr[2])
+                self.dict[arr[2]]=b
+                PC+=1
+        elif(arr[0]=='mov'):
+            if(type(arr[2]!=int)):
+                b=self.bintodec(arr[1])
+                c=""
+                c=self.dectobin(b)
+                self.dict[arr[2]]=c   
+                PC+=1 
+        # elif(arr[0]=='ld'):
+        # elif(arr[0]=='st'):
+        elif(arr[0]=='mul'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            c=dectobin(a*b)
+            self.dict[arr[3]]=c
+            PC+=1
+        elif(arr[0]=='div'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            d=""
+            c=dectobin(a/b)
+            d=dectobin(a%b)
+            self.dict['000']=c
+            self.dict['001']=d
+            PC+=1
+        # elif(arr[0]=='rs'):
+        # elif(arr[0]=='ls'):
+        elif(arr[0]=='xor'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            c=dectobin(a^b)
+            self.dict[arr[3]]=c 
+            PC+=1
+        elif(arr[0]=='or'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            c=dectobin(a|b)
+            self.dict[arr[3]]=c 
+            PC+=1
+        elif(arr[0]=='and'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            c=""
+            c=dectobin(a&b)
+            self.dict[arr[3]]=c 
+            PC+=1  
+        elif(arr[0]=='not'):
+            a=self.bintodec(arr[1])
+            c=""
+            c=dectobin(~a)
+            self.dict[arr[3]]=c
+            PC+=1
+        elif(arr[0]=='cmp'):
+            a=self.bintodec(arr[1])
+            b=self.bintodec(arr[2])
+            if(a>b):
+                self.dict['111'][14]='1'
+                PC+=1
+            elif(a<b):
+                self.dict['111'][13]='1' 
+                PC+=1           
+            elif(a==b):
+                self.dict['111'][15]='1'
+                PC+=1
+        elif(arr[0]=='jmp'):
+            pcnew=int(arr[2])
+            PC=pcnew
+        elif(arr[0]=='jlt'):
             
-        
+                    
+                    
             
                         
