b627af0f20504f74050360afae2b2f48021f2dd5
Aarav Balachandran <97381152+AaravBalachandran@users.noreply.github.com>
2022-07-23 14:42:53 +0530

    simulator

--- simpleSimulator/Decode.py
+++ simpleSimulator/Decode.py
@@ -2,11 +2,15 @@ from ast import Return
 
 
 class Decoder:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,isa_dict,unUsedBitsTable):
+        self.isa_dict=isa_dict
+        self.unUsedBitsTable=unUsedBitsTable
     def decode(self,instruction):
         a=[]
-        
+        opcode=instruction[:5]
+        # func=self.isa_dict[opcode]
+        type=self.isa_dict[opcode]
+        unusedbits=self.unUsedBitsTable[type]        
         return a
     
 # #1010100100000111
