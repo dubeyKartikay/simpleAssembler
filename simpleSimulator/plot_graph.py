20b2c16f6e02d333da021fe05dfd2ba6c1a5f867
Aarav Balachandran <97381152+AaravBalachandran@users.noreply.github.com>
2022-08-06 17:17:44 +0530

    graph2

--- simpleSimulator/plot_graph.py
+++ simpleSimulator/plot_graph.py
@@ -1,2 +1,13 @@
 import matplotlib.pyplot as plt
 import math as m
+cycleN=[]
+memAdd=[]
+def plot_graph(cycleN,memAdd):
+    
+    print("\n")
+    plt.scatter(cycleN, memAdd, color='black')
+    plt.title('Scatter Plot Graph', fontsize=20)
+    plt.xlabel('Cycle Number', fontsize=20)
+    plt.ylabel('Memory Adress', fontsize=20)
+    plt.show()
+    
