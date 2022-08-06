d8cff2e6785532341ea25816a823fc2e9a32ae18
Aarav Balachandran <97381152+AaravBalachandran@users.noreply.github.com>
2022-08-06 23:18:18 +0530

    fbjdhs

--- Q5/main.py
+++ Q5/main.py
@@ -1,6 +1,6 @@
 from math import log2
-space_mem = input("input memory space : ")
-type_addresable = input("input addressable memory type : ")
+space_mem = input("Input memory space : ")
+type_addresable = input("Input addressable memory type : ")
 y=space_mem[-5:]
 if space_mem[-2:] == "mB":
     space_mem = int(space_mem[:-2])*((2**20)*8)
@@ -26,23 +26,23 @@ d = {
 }
 mem_slots = space_mem/d[type_addresable]
 bits_required = log2(mem_slots)
-query_type = int(input("input the type of question (1/2) : "))
+query_type = int(input("Input the type of question (1/2) : "))
 if query_type == 0:
-    lenofins = int(input("input length of one instruction : "))
-    lenofreg = int(input("input length of registers : "))
-    print("bits required to represent an address is : ",bits_required)
+    lenofins = int(input("Input the length of 1 instruction : "))
+    lenofreg = int(input("Input the length of registers : "))
+    print("Bits required to represent an address is : ",bits_required)
     q = lenofins-lenofreg-bits_required
-    print("bits needed by opcode : ",q)
-    print("filler bits in instruction type 2 : ",lenofins-2*lenofreg-q)
+    print("Bits needed by opcode : ",q)
+    print("Filler bits in instruction type 2 : ",lenofins-2*lenofreg-q)
     M_I=2**q
-    print("Maximum numbers of instructions this ISA can support : ",M_I)
+    print("Maximum number of instructions this ISA can support : ",M_I)
     M_R=2**lenofreg
     print("Maximum number of registers this ISA can support : ",M_R)
 else:
-    x=int(input("Enter the Type (1/2) : "))
+    x=int(input("Enter the Sub-Type (1/2) : "))
     if x==1:
-        cpu_bit = int(input("how many bits the cpu is : "))
-        new_type_addresable = input("input new addressable memory type : ")
+        cpu_bit = int(input("How many bits the cpu is : "))
+        new_type_addresable = input("Input new addressable memory type : ")
         d['Word']=cpu_bit
         if(y=='kWord' or y=='mWord'):
             space_mem=space_mem*cpu_bit
@@ -56,13 +56,13 @@ else:
         if(otpt<0):
             flag=1
         if flag==1:
-            print(abs(otpt)," pins saved")
+            print(abs(otpt),"Pins saved")
         elif flag==0:
-            print(abs(otpt)," pins required")
+            print(abs(otpt),"Pins required")
     elif x==2:
-        cpu_bit = int(input("how many bits the cpu is : "))
+        cpu_bit = int(input("How many bits the cpu is : "))
         pins= int(input("Enter adress pins : "))
-        new_type_addresable = input("input new addressable memory type : ")
+        new_type_addresable = input("Input new addressable memory type : ")
         if(new_type_addresable=='Word'):
             d['Word']=cpu_bit
         total=(2**pins)*d[new_type_addresable]/d[type_addresable]
