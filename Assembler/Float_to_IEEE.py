8caa512ad9dcc6a881b85b386df9c297b0d87945
mananchugh02 <99235312+mananchugh02@users.noreply.github.com>
2022-08-06 15:06:08 +0530

    errors handled

--- Assembler/Float_to_IEEE.py
+++ Assembler/Float_to_IEEE.py
@@ -22,27 +22,30 @@ def float2bin(float):
         j=j+k
     j=str(j)
     return j
-n=31.5
-exp_final=""
-mantissa_final=""
-int_part=int(n)
-float_part=n-int_part
-bias=0
-float_bin=float2bin(float_part)
-int_bin=dectobin(int_part)
-n=0
-for i in range(len(int_bin)):
-    if int_bin[i]=='1':
-        break
-    n+=1
-int_bin1=int_bin[n:len(int_bin)+1]
-float_extra=int_bin[1:len(int_bin)+1]
-exp=len(int_bin1)-1
-exp_dec=bias+exp
-exp_bin=dectobin(exp_dec)
-mantissa=float_extra+float_bin
-exp_final+="0"*(3-len(exp_bin))
-exp_final+=exp_bin
-mantissa_final=mantissa+"0"*(5-len(mantissa))
-finalotpt=exp_final+mantissa_final
-print(finalotpt)
+def final(n):
+    exp_final=""
+    mantissa_final=""
+    int_part=int(n)
+    float_part=n-int_part
+    bias=0
+    float_bin=float2bin(float_part)
+    int_bin=dectobin(int_part)
+    n=0
+    for i in range(len(int_bin)):
+        if int_bin[i]=='1':
+            break
+        n+=1
+    int_bin1=int_bin[n:len(int_bin)+1]
+    float_extra=int_bin[1:len(int_bin)+1]
+    exp=len(int_bin1)-1
+    exp_dec=bias+exp
+    exp_bin=dectobin(exp_dec)
+    mantissa=float_extra+float_bin
+    exp_final+="0"*(3-len(exp_bin))
+    exp_final+=exp_bin
+    mantissa_final=mantissa+"0"*(5-len(mantissa))
+    finalotpt=exp_final+mantissa_final
+    print(finalotpt)
+
+
+final(31.5)
