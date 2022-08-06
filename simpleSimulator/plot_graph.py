312d122352bdc1d551320cd515b71ec8c6828f80
Aarav Balachandran <97381152+AaravBalachandran@users.noreply.github.com>
2022-08-06 22:00:48 +0530

    graph21

--- simpleSimulator/plot_graph.py
+++ simpleSimulator/plot_graph.py
@@ -1,14 +1,11 @@
 import matplotlib.pyplot as plt
 import math as m
-import q2copy
-cycleN,memAddress=q2copy.sim()
 
 def plot_graph(cycleN,memAddress):
     
     print("\n")
-    plt.scatter(cycleN,memAddress, color='blue')
-    plt.title('Scatter Plot Graph', fontsize=20)
-    plt.xlabel('Cycle Number', fontsize=20)
-    plt.ylabel('Memory Adress', fontsize=20)
+    plt.scatter(cycleN,memAddress, color='black')
+    plt.title('SCATTER PLOT', fontsize=10)
+    plt.xlabel('Cycle Number ->', fontsize=10)
+    plt.ylabel('Memory Address ->', fontsize=10)
     plt.show()
-    
